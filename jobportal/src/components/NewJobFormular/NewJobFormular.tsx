98a104fde9dd6b1ab34c5bbd044478c862b2740c
Vitali Schumarov <v.schumarov@icloud.com>
2025-05-22 21:03:29 +0200

    created the formular page

--- jobportal/src/components/NewJobFormular/NewJobFormular.tsx
+++ jobportal/src/components/NewJobFormular/NewJobFormular.tsx
@@ -4,7 +4,12 @@ export default function NewJobFormular() {
   return (
     <div>
       <Link to={"/home"}>Back</Link>
-      <h3>here is the formular!</h3>
+      <h3>Title of the job:</h3>
+      <input type="text" placeholder="title..." />
+      <h3>Jobdescription:</h3>
+      <input type="text" placeholder="describe the job..." />
+      <h3>What will the salary be:</h3>
+      <input type="number" /> <span>CHF</span>
     </div>
   );
 }
