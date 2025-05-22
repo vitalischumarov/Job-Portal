0d1b0675d5918bb7040d75d94c0a646bbd078c78
Vitali Schumarov <v.schumarov@icloud.com>
2025-05-22 21:35:03 +0200

    type input handler implemented for new jobs

--- jobportal/src/components/NewJobFormular/NewJobFormular.tsx
+++ jobportal/src/components/NewJobFormular/NewJobFormular.tsx
@@ -1,15 +1,50 @@
 import { Link } from "react-router-dom";
 
+import { useState } from "react";
 export default function NewJobFormular() {
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [salary, setSalary] = useState<number>(0);
+
+  function typeTitleHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    setTitle(event.target.value);
+  }
+
+  function typeDescriptionHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    setDescription(event.target.value);
+  }
+
+  function typeSalaryHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    setSalary(Number(event.target.value));
+  }
+
   return (
     <div>
       <Link to={"/home"}>Back</Link>
       <h3>Title of the job:</h3>
-      <input type="text" placeholder="title..." />
+      <input
+        type="text"
+        placeholder="title..."
+        name="title"
+        value={title}
+        onChange={typeTitleHandler}
+      />
       <h3>Jobdescription:</h3>
-      <input type="text" placeholder="describe the job..." />
+      <input
+        type="text"
+        placeholder="describe the job..."
+        name="description"
+        value={description}
+        onChange={typeDescriptionHandler}
+      />
       <h3>What will the salary be:</h3>
-      <input type="number" /> <span>CHF</span>
+      <input
+        type="number"
+        name="salary"
+        value={salary}
+        onChange={typeSalaryHandler}
+      />{" "}
+      <span>CHF</span>
     </div>
   );
 }
